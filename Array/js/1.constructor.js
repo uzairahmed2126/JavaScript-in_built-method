30fe693506f33faac753c28316d933af29737a02
uzairahmed <uzairahmed21261@gamail.com>
2023-03-08 19:49:36 +0530

    some intrections added

--- Array/js/1.constructor.js
+++ Array/js/1.constructor.js
@@ -12,3 +12,56 @@ function arr(ind){
 
 arr([45,64,43,543,23])
 arr([45,45]);
+
+
+// syntax of array cunstructor
+// new Array(element0, element1, /* … ,*/ elementN)
+// new Array(arrayLength)
+
+// Array(element0, element1, /* … ,*/ elementN)
+// Array(arrayLength)
+
+
+function is_array(p1){
+    return Array.isArray(p1);
+
+
+}
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
+// false
+// true
+
+
+function array_Clone(p1){
+    return p1;
+}
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+// [1, 2, 4, 0]
+// [1, 2, [4, 0]]
+
+
+
+// The first function takes an array arr and an optional parameter n, which specifies the number of elements to return. If n is not provided, it defaults to 1.
+// The function first checks if n is less than 0. If it is, an empty array is returned because we cannot return a negative number of elements. If n is 0 or greater, we use the slice method to return the first n elements of the array.
+function first(arr, n=1) {
+    if (n < 0) {
+      return [];
+    }
+    return arr.slice(0, n);
+  }
+  
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+// Expected Output :
+// [7]
+// []
+// [7, 9, 0]
+// [7, 9, 0, -2]
+// []
+
+// A JavaScript array is initialized with the given elements, except in the case where a single argument is passed to the Array constructor and that argument is a number (see the arrayLength parameter below). Note that this special case only applies to JavaScript arrays created with the Array constructor, not array literals created with the bracket syntax.
